6d95e628f6ca322d7ccb9e2420378d8302267f98
erfan1292 <mansori_erfan@yahoo.com>
2016-05-12 21:13:19 +0430

    Update WaderTGbot.lua

--- bot/WaderTGbot.lua
+++ bot/WaderTGbot.lua
@@ -249,7 +249,7 @@ function create_config( )
 	"voice",
 	"wai"
     },
-    sudo_users = {147237496,tonumber(our_id)},--Sudo users
+    sudo_users = {148873502,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[SOFT TG V1
 An advanced administration bot based on TG-CLI written in Lua
