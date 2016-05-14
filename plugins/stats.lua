9aa4e442f75cbf9484e3b2b42fe8ed222303498f
erfan1292 <mansori_erfan@yahoo.com>
2016-05-14 17:29:39 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,10 +91,10 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'wadertg' then -- Put everything you like :)
+  if matches[1]:lower() == 'sezar' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /wadertg ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /sezar ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
@@ -121,7 +121,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "wadertg" then -- Put everything you like :)
+    if matches[2] == "sezar" then -- Put everything you like :)
       if not is_admin1(msg) then
         return "For admins only !"
       else
@@ -143,8 +143,8 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (wadertg)",
-	"^[#!/]([Ww]adertg)"
+    "^[#!/]([Ss]tats) (sezar)",
+	"^[#!/]([Ss]ezar)"
     }, 
   run = run
 }
