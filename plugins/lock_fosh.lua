de25f8dbc0bb53688920b20228c5f40c60c74607
erfan1292 <mansori_erfan@yahoo.com>
2016-05-30 22:46:45 +0430

    Update lock_fosh.lua

--- plugins/lock_fosh.lua
+++ plugins/lock_fosh.lua
@@ -5,8 +5,8 @@ local function run(msg, matches)
     local data = load_data(_config.moderation.data)
     if data[tostring(msg.to.id)] then
         if data[tostring(msg.to.id)]['settings'] then
-            if data[tostring(msg.to.id)]['settings']['lock_fosh'] then
-                lock_fosh = data[tostring(msg.to.id)]['settings']['lock_fosh']
+            if data[tostring(msg.to.id)]['settings']['fosh'] then
+                lock_fosh = data[tostring(msg.to.id)]['settings']['fosh']
             end
         end
     end
@@ -34,7 +34,6 @@ return {
 	"گوساله",
 	"gosale",
 	"gusale",
-	"خر",
   },
   run = run
 }
