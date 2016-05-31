d62dd2b1a2339d11b249d6c61b5488d0fe10304b
erfan1292 <mansori_erfan@yahoo.com>
2016-05-31 12:16:23 +0430

    Update supergroup.lua

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -966,7 +966,7 @@ function show_supergroup_settingsmod(msg, target)
   local gp_type = data[tostring(msg.to.id)]['group_type']
   
   local settings = data[tostring(target)]['settings']
-  local text = "\n⚙SuperGroup settings⚙:️\n>Lock links : "..settings.lock_link.."\n>Lock contacts: "..settings.lock_contacts.."\n>Lock flood: "..settings.flood.."\n>Flood sensitivity : "..NUM_MSG_MAX.."\n>Lock spam: "..settings.lock_spam.."\n>Lock Arabic: "..settings.lock_arabic.."\n>Lock Member: "..settings.lock_member.."\n>Lock RTL: "..settings.lock_rtl.."\n>Lock Tgservice: "..settings.lock_tgservice.."\n>Lock sticker: "..settings.lock_sticker.."\n>Lock tag: "..settings.tag.."\n>Lock media: "..settings.media.."\n>Lock fosh: "..settings.fosh.."\n>Lock bots: "..bots_protection.."\n>Lock operator: "..settings.operator.."\n>Group Public: "..settings.public.."\n>Strict settings: "..settings.strict.."\n=============="
+  local text = "\n⚙SuperGroup settings⚙:️\n>Lock links : "..settings.lock_link.."\n>Lock contacts: "..settings.lock_contacts.."\n>Lock flood: "..settings.flood.."\n>Flood sensitivity : "..NUM_MSG_MAX.."\n>Lock spam: "..settings.lock_spam.."\n>Lock Arabic: "..settings.lock_arabic.."\n>Lock Member: "..settings.lock_member.."\n>Lock RTL: "..settings.lock_rtl.."\n>Lock Tgservice: "..settings.lock_tgservice.."\n>Lock sticker: "..settings.lock_sticker.."\n>Lock tag: "..settings.tag.."\n>Lock media: "..settings.media.."\n>Lock fosh: "..settings.fosh.."\n>Lock bots: "..bots_protection.."\n>Lock operator: "..settings.operator.."\n>Group Public: "..settings.public.."\n>Strict settings: "..settings.strict.."
   return text
 end
 
