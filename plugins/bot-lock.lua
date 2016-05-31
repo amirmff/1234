55da3982acae1c57a1c66aa176987cd76e6a50ba
erfan1292 <mansori_erfan@yahoo.com>
2016-05-31 12:55:23 +0430

    Update bot-lock.lua

--- plugins/bot-lock.lua
+++ plugins/bot-lock.lua
@@ -1,44 +1,53 @@
-local function run(msg, matches)
-    if is_momod(msg) then
-        return
+local function run (msg, matches)
+
+  if matches[1] ~= 'chat_add_user' and matches[1] ~= 'chat_add_user_link' then
+    if msg.to.type ~= 'chat' and msg.to.type ~= 'channel' then
+      return nil
     end
-    local data = load_data(_config.moderation.data)
-    if data[tostring(msg.to.id)] then
-        if data[tostring(msg.to.id)]['settings'] then
-            if data[tostring(msg.to.id)]['settings']['lock_bots'] then
-                lock_fosh = data[tostring(msg.to.id)]['settings']['lock_bots']
-            end
+  end
+
+  local chatId = msg.to.id
+  if matches[1] == 'lock' then
+    enableAntiBot(chatId)
+  return 'bot has been locked'
+  end
+  if matches[1] == 'unlock' then
+    disableAntiBot(chatId)
+  return 'bot has been unlocked'
+  end
+  if matches[1] == 'chat_add_user' or matches[1] == 'chat_add_user_link' then
+    local user = msg.action.user or msg.from
+    if isABotBadWay(user) then
+      print("It' a bot!")
+      if isAntiBotEnabled(chatId) then
+        print('bot is locked')
+        local userId = user.id
+        if not isBotAllowed(userId, chatId) then
+          kickUser(userId, chatId)
+        else
+          print('')
         end
+      end
     end
-local function isAntiBotEnabled (chatId)
-  local hash = 'bot:lock:'..chatId
-  local lock = redis:get(hash)
-  return lock
-end
-local function enableAntiBot (chatId)
-local hash = 'bot:lock:'..chatId
-redis:set(hash, true)
-end
-local function disableAntiBot (chatId)
-local hash = 'bot:lock:'..chatId
-redis:del(hash)
-end
-local function isABot (user)
-  local binFlagIsBot = 4096
-  local result = bit32.band(user.flags, binFlagIsBot)
-  return result == binFlagIsBot
+  end
 end
 
-local function isABotBadWay (user)
-  local username = user.username or ''
-  return username:match("[Bb]ot$")
-end
-local function kickUser(userId, chatId)
-local channel = 'channel#id'..chatId
-local user = 'user#id'..userId
-  channel_kick_user(channel, user, function (data, success, result)
-    if success ~= 1 then
-      print('I can\'t kick '..data.user..' but should be kicked')
-    end
-  end, {chat=chat, user=user})
-end
+return {
+  description = 'Anti bot',
+  usage = {
+    '[/!#]lock bot: locked add bots to supergroup',
+    '[/!#]unlock bot: unlock add bots to supergroup'
+  },
+  patterns = {
+    '^[/!#](lock) bot$',
+    '^[/!#](unlock) bot$',
+    '^!!tgservice (chat_add_user)$',
+    '^!!tgservice (chat_add_user_link)$'
+  },
+  run = run
+}
+
+--shared by @WaderTGTeam
+--by @WaderTG
+Status API Training Shop Blog About
+© 2016 GitHub, Inc. Terms Privacy Securit
