91ac20ff1050c2d59dccc70dd84165584232d4a7
erfan1292 <mansori_erfan@yahoo.com>
2016-05-31 12:50:14 +0430

    Update bot-lock.lua

--- plugins/bot-lock.lua
+++ plugins/bot-lock.lua
@@ -1,3 +1,15 @@
+local function run(msg, matches)
+    if is_momod(msg) then
+        return
+    end
+    local data = load_data(_config.moderation.data)
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['lock_bots'] then
+                lock_fosh = data[tostring(msg.to.id)]['settings']['lock_bots']
+            end
+        end
+    end
 local function isAntiBotEnabled (chatId)
   local hash = 'bot:lock:'..chatId
   local lock = redis:get(hash)
@@ -30,55 +42,3 @@ local user = 'user#id'..userId
     end
   end, {chat=chat, user=user})
 end
-
-local function run (msg, matches)
-
-  if matches[1] ~= 'chat_add_user' and matches[1] ~= 'chat_add_user_link' then
-    if msg.to.type ~= 'chat' and msg.to.type ~= 'channel' then
-      return nil
-    end
-  end
-
-  local chatId = msg.to.id
-  if matches[1] == 'lock' then
-    enableAntiBot(chatId)
-  return 'bot has been locked'
-  end
-  if matches[1] == 'unlock' then
-    disableAntiBot(chatId)
-  return 'bot has been unlocked'
-  end
-  if matches[1] == 'chat_add_user' or matches[1] == 'chat_add_user_link' then
-    local user = msg.action.user or msg.from
-    if isABotBadWay(user) then
-      print("It' a bot!")
-      if isAntiBotEnabled(chatId) then
-        print('bot is locked')
-        local userId = user.id
-        if not isBotAllowed(userId, chatId) then
-          kickUser(userId, chatId)
-        else
-          print('')
-        end
-      end
-    end
-  end
-end
-
-return {
-  description = 'Anti bot',
-  usage = {
-    '[/!#]lock bot: locked add bots to supergroup',
-    '[/!#]unlock bot: unlock add bots to supergroup'
-  },
-  patterns = {
-    '^[/!#](lock) bot$',
-    '^[/!#](unlock) bot$',
-    '^!!tgservice (chat_add_user)$',
-    '^!!tgservice (chat_add_user_link)$'
-  },
-  run = run
-}
-
---shared by @WaderTGTeam
---by @WaderTG
