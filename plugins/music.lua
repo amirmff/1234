0bb89bb79a14a946b83f08d5535d1193f8b79a20
erfan1292 <mansori_erfan@yahoo.com>
2016-06-08 16:37:40 +0430

    Update music.lua

--- plugins/music.lua
+++ plugins/music.lua
@@ -3,7 +3,7 @@ local function musiclink(msg, musicid)
 	if not value then
 		return
 	else
-		value = value..'\nFire Bot'
+		value = value..'\nsezar.bot'
 		return value
 	end
 end
@@ -36,7 +36,7 @@ function run(msg, matches)
 		if not value then
 			return 'آهنگ مورد نظر پیدا نشد.'
 		else
-			value = value..'\n\nFire Bot'
+			value = value..'\n\nsezar.bot'
 			return value
 		end
 		return
